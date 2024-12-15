59ecf4e6cd6a7da69820f8db86ea583bb45c1ca3
khanhleg2003 <lythatda942003@gmail.com>
2024-12-15 22:48:22 +0700

    -m

--- main/src/main/java/main/main/Entity/UserEntity.java
+++ main/src/main/java/main/main/Entity/UserEntity.java
@@ -1,22 +1,21 @@
 package main.main.Entity;
 
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class UserEntity {
    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
 
     private String name;
-
     private String email;
     
 
