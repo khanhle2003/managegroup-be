a677372e054fed0bc32b5090182b81afd41454d2
khoa <khoadzvlhuhu@gmail.com>
2024-12-19 11:11:02 +0700

    API loginout

--- main/src/main/java/main/main/Controller/user_login_logout_/AllArgsContructor.java
+++
@@ -1,4 +0,0 @@
-package main.main.Controller.user_login_logout_;
-
-public @interface AllArgsContructor {
-}
---
+++ main/src/main/java/main/main/jwtauth/service/UserService.java
@@ -0,0 +1,48 @@
+package main.main.jwtauth.service;
+
+import main.main.jwtauth.model.User;
+import main.main.jwtauth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+
+@Service
+public class UserService implements UserDetailsService{
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return Optional.ofNullable(userRepository.findByUsername(username));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
