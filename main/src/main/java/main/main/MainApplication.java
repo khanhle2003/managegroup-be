4c619759e3374862bd4031d27c4257f6ef26c241
khanhleg2003 <lythatda942003@gmail.com>
2024-12-24 16:21:24 +0700

    test

--- main/src/main/java/main/main/MainApplication.java
+++ main/src/main/java/main/main/MainApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@ComponentScan(basePackages  = "main.main.exportData")
+@SpringBootApplication(scanBasePackages = "main.main")
+// @ComponentScan(basePackages  = "main.main.exportData")
 @RestController
 public class MainApplication {
 	public static void main(String[] args) {
