31bcb5d9f41cec8f90798e3653b7a748d73c51cb
nguyenlongnt270705 <146830417+nguyenlongnt270705@users.noreply.github.com>
2024-12-25 16:13:37 +0700

    Update UserController.java

--- main/src/main/java/main/main/jwtauth/controller/UserController.java
+++ main/src/main/java/main/main/jwtauth/controller/UserController.java
@@ -3,40 +3,25 @@ package main.main.jwtauth.controller;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import main.main.jwtauth.model.User;
-import main.main.jwtauth.service.UserService;
-
 @Controller
 @RequestMapping("/users")
+@CrossOrigin(origins = "*")
 public class UserController {
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/download")
     public ResponseEntity<ByteArrayResource> downloadUserList() throws IOException {
-        List<User> users = userService.findAllUsers();
-
         // Đọc file template DOCX
-        try (XWPFDocument document = new XWPFDocument(new FileInputStream("src/main/resources/templates/don_xin_di_nuoc_ngoai.docx"))) {
-            XWPFParagraph paragraph = document.createParagraph();
-            paragraph.createRun().setText("Danh sách người dùng:");
-
-            for (User user : users) {
-                paragraph.createRun().setText("ID: " + user.getId() + ", Tên: " + user.getUsername() + ", Email: " + user.getEmail());
-            }
-
+        try (XWPFDocument document = new XWPFDocument(new FileInputStream("C:\\Users\\Admin\\Desktop\\New folder (4)\\managegroup-be-main\\main\\src\\main\\resources\\templates\\don_xin_di_nuoc_ngoai_test.docx"))) {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             document.write(outputStream);
             ByteArrayResource resource = new ByteArrayResource(outputStream.toByteArray());
