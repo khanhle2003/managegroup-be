c3dc21cb86862696e66e803e6ee70618a0640e15
khanhleg2003 <lythatda942003@gmail.com>
2024-12-27 10:07:50 +0700

    import data form word

--- main/src/main/java/main/main/AddData/entity/Data.java
+++ main/src/main/java/main/main/AddData/entity/Data.java
@@ -2,6 +2,8 @@ package main.main.AddData.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Data {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "FullName")
     private String FullName;
--- main/src/main/java/main/main/exportData/config/CorsConfig.java
+++ main/src/main/java/main/main/exportData/config/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
