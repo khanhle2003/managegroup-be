5c0e671dc4768a0825f16860a9a46502aa89111c
nguyenlongnt270705 <146830417+nguyenlongnt270705@users.noreply.github.com>
2025-01-06 10:37:17 +0700

    Update listDoan.java

--- main/src/main/java/main/main/jwtauth/model/listDoan.java
+++ main/src/main/java/main/main/jwtauth/model/listDoan.java
@@ -1,3 +1,4 @@
+/*
 package main.main.jwtauth.model;
 
 import jakarta.persistence.Column;
@@ -217,5 +218,61 @@ public class listDoan {
 
     public void setEndDate(Date endDate) {
         this.endDate = endDate;
+    }
+}
+*/
+
+package main.main.jwtauth.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "data")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter và Setter cho username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter và Setter cho password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Getter và Setter cho email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
