7adcbb3b77c9b740da9952b6c4d2b65166aceb0c
nguyenlongnt270705 <146830417+nguyenlongnt270705@users.noreply.github.com>
2025-01-07 15:41:01 +0700

    Update User.java

--- main/src/main/java/main/main/jwtauth/model/User.java
+++ main/src/main/java/main/main/jwtauth/model/User.java
@@ -4,16 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
     private String email;
-
 
     public Long getId() {
         return id;
