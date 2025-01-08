4c8d115c99a5aa98e9122d6f354936161b42577c
Danh dang hoc <ldanh3113@gmail.com>
2025-01-08 15:28:14 +0700

    ll

--- main/src/main/java/main/main/jwtauth/model/listDoan.java
+++ main/src/main/java/main/main/jwtauth/model/listDoan.java
@@ -262,7 +262,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "data")
 public class listDoan {
@@ -330,183 +334,14 @@ public class listDoan {
     @Column(name = "Gender")
     private String gender;
 
+    @Column(name = "email")
+    private String email;
+
+    @Column(name ="phoneNumber")
+    private String phoneNumber;
+
     @Column(name = "Position")
     private String position;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getTripPurpose() {
-        return tripPurpose;
-    }
-
-    public void setTripPurpose(String tripPurpose) {
-        this.tripPurpose = tripPurpose;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public String getSelfFunded() {
-        return selfFunded;
-    }
-
-    public void setSelfFunded(String selfFunded) {
-        this.selfFunded = selfFunded;
-    }
-
-    public String getSponsor() {
-        return sponsor;
-    }
-
-    public void setSponsor(String sponsor) {
-        this.sponsor = sponsor;
-    }
-
-    public String getHospital() {
-        return hospital;
-    }
-
-    public void setHospital(String hospital) {
-        this.hospital = hospital;
-    }
-
-    public String getHdBc() {
-        return hdBc;
-    }
-
-    public void setHdBc(String hdBc) {
-        this.hdBc = hdBc;
-    }
-
-    public String getInvitationUnit() {
-        return invitationUnit;
-    }
-
-    public void setInvitationUnit(String invitationUnit) {
-        this.invitationUnit = invitationUnit;
-    }
-
-    public String getDoan() {
-        return doan;
-    }
-
-    public void setDoan(String doan) {
-        this.doan = doan;
-    }
-
-    public String getPartyMember() {
-        return partyMember;
-    }
-
-    public void setPartyMember(String partyMember) {
-        this.partyMember = partyMember;
-    }
-
-    public String getForeignTripCount() {
-        return foreignTripCount;
-    }
-
-    public void setForeignTripCount(String foreignTripCount) {
-        this.foreignTripCount = foreignTripCount;
-    }
-
-    public String getNotificationNumber() {
-        return notificationNumber;
-    }
-
-    public void setNotificationNumber(String notificationNumber) {
-        this.notificationNumber = notificationNumber;
-    }
-
-    public String getNotificationDate() {
-        return notificationDate;
-    }
-
-    public void setNotificationDate(String notificationDate) {
-        this.notificationDate = notificationDate;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
 }
 
