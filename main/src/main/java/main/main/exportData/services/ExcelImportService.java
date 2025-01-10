230e6400688fcb29ab4423e4252e1c98662c1b61
khoa <khoadzvlhuhu@gmail.com>
2025-01-10 16:40:12 +0700

    import

---
+++ main/src/main/java/main/main/exportData/services/ExcelImportService.java
@@ -0,0 +1,109 @@
+package main.main.exportData.services;
+
+import io.jsonwebtoken.io.IOException;
+import jakarta.transaction.Transactional;
+import main.main.exportData.Entity.DataofCusEntity;
+import main.main.exportData.Repo.DataofCusRepo;
+import main.main.jwtauth.repository.listDoanRepo;
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+@Transactional
+public class ExcelImportService {
+    @Autowired
+    private DataofCusRepo dataofCusRepo;
+
+    public void importFromExcel(MultipartFile file) throws IOException, java.io.IOException {
+        try (Workbook workbook = WorkbookFactory.create(file.getInputStream())){
+            Sheet sheet = workbook.getSheetAt(0);
+
+            Iterator<Row> rows = sheet.iterator();
+            rows.next();
+
+            List<DataofCusEntity> listDoans = new ArrayList<>();
+
+            while (rows.hasNext()){
+                Row row = rows.next();
+
+                DataofCusEntity listdoan = new DataofCusEntity();
+                listdoan.setFullName(getCellValue(row.getCell(0)));
+                listdoan.setBirth_date(parseDate(getCellValue(row.getCell(1))));
+                listdoan.setGender(getCellValue(row.getCell(2)));
+                listdoan.setPartyBranch(getCellValue(row.getCell(3)));
+                listdoan.setPartyPosition(getCellValue(row.getCell(4)));
+                listdoan.setJobTitle(getCellValue(row.getCell(5)));
+                listdoan.setJobName(getCellValue(row.getCell(6)));
+                listdoan.setUnit(getCellValue(row.getCell(7)));
+                listdoan.setPhoneNumber(getCellValue(row.getCell(8)));
+                listdoan.setEmail(getCellValue(row.getCell(9)));
+                listdoan.setCountry(getCellValue(row.getCell(10)));
+                listdoan.setInvitationUnit(getCellValue(row.getCell(11)));
+                listdoan.setMoiDichDanh(getCellValue(row.getCell(12)));
+                listdoan.setTripPurpose(getCellValue(row.getCell(13)));
+                listdoan.setStartDate(parseDate(getCellValue(row.getCell(14))));
+                listdoan.setMonthBegon(getCellValue(row.getCell(15)));
+                listdoan.setEndDate(parseDate(getCellValue(row.getCell(16))));
+                listdoan.setThoigiandichuyen(getCellValue(row.getCell(17)));
+                listdoan.setSelfFunded(getCellValue(row.getCell(18)));
+                listdoan.setSponsor(getCellValue(row.getCell(19)));
+                listdoan.setHospital(getCellValue(row.getCell(20)));
+                listdoan.setGiaTri(getCellValue(row.getCell(21)));
+                listdoan.setForeignTripCount(getCellValue(row.getCell(22)));
+                listdoan.setNgayXindi(parseDate(getCellValue(row.getCell(23))));
+                listdoan.setNgayPnhanHS(parseDate(getCellValue(row.getCell(24))));
+                listdoan.setNotificationNumber(getCellValue(row.getCell(25)));
+                listdoan.setNotificationDate(parseDate(getCellValue(row.getCell(26))));
+                listdoan.setNgaychuyenHSsangP(parseDate(getCellValue(row.getCell(27))));
+                listdoan.setAlternative(getCellValue(row.getCell(28)));
+                listdoan.setSoNghiPhep(getCellValue(row.getCell(29)));
+                listdoan.setNgayNghiPhep(parseDate(getCellValue(row.getCell(30))));
+                listdoan.setSubmitDay(getCellValue(row.getCell(31)));
+                listdoan.setPhotoHochieu(parseDate(getCellValue(row.getCell(32))));
+                listdoan.setNoiDung(getCellValue(row.getCell(33)));
+                listdoan.setTenBaoCao(getCellValue(row.getCell(34)));
+                listdoan.setHoanHuy(getCellValue(row.getCell(35)));
+                listdoan.setKhac(getCellValue(row.getCell(36)));
+
+                dataofCusRepo.save(listdoan);
+
+            }
+        }
+    }
+
+    private String getCellValue(Cell cell) {
+        if (cell == null){
+            return "";
+        }
+        switch (cell.getCellType()){
+            case STRING:
+                return cell.getStringCellValue();
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    return cell.getLocalDateTimeCellValue().toString();
+                }
+                return String.valueOf(cell.getNumericCellValue());
+            default:
+                return "";
+        }
+    }
+
+    private LocalDate parseDate(String dateStr) {
+        try {
+            LocalDate localDate = LocalDate.parse(dateStr);
+
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
+    }
+}
